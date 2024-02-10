423d8a58acf2e73f157bc70d5d6c5807676f7b1b
Clark <gabriel.gmcontato@gmail.com>
2024-02-10 13:52:17 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,51 @@
+const form = document.getElementById('form-deposito');
+const campoA = document.getElementById('campoA');
+const campoB = document.getElementById('campoB');
+let formEValido = false
+
+function validaCampo(campoA, campoB) {
+    if(campoA.value < campoB.value){
+        return true
+    }else{
+        return false
+    }
+    
+}
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    
+    const mensagemSucesso = `O valor <b>${campoB.value}</b> é maior que <b>${campoA.value}</b>`;
+
+    formEValido = validaCampo(campoA,campoB)
+    if(formEValido){
+        const containerMensagemSucesso = document.querySelector('.success-message');
+        containerMensagemSucesso.innerHTML = mensagemSucesso;
+        containerMensagemSucesso.style.display = 'block';
+
+        campoA.value = '';
+        campoB.value = '';
+
+    }else{
+        campoB.style.border = '1px solid red';
+        document.querySelector('.error-message').style.display = 'block';
+    }
+})
+
+campoB.addEventListener('keyup', function(e){
+    console.log(e.target.value);
+    formEValido = validaCampo(campoA, campoB)
+
+    if (!formEValido){
+        //campoB.classList.add('error');
+        campoB.style.border = '1px solid red';
+        document.querySelector('.error-message').style.display = 'block';
+    }else{
+        //campoB.classList.remove('error');
+        campoB.style = '';
+        document.querySelector('.error-message').style.display = 'none';
+    }
+})
+
+
+
